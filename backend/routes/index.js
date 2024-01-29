1eaf2534ada976a25dfdbb7f628fea27e77695bf
Abuassefa7 <abuassefa19@gmail.com>
2024-01-29 13:09:59 -0600

    employeebackend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,12 @@ let router = express.Router();
 
 let installRouter = require("./install.routes");
 
-// const employeeRouter = require("./employe.routes");
+const employeeRouter = require('./employee.routes');
 // const loginRoutes = require("./login.routes");
 
 
 router.use(installRouter);
-// router.use(employeeRouter);
+router.use(employeeRouter);
 // router.use(loginRoutes);
 
 module.exports = router;
