95ebf51cb0127fe85c757d56200874b3cc440e58
temu man <talemie@yahoo.com>
2024-01-30 12:06:03 -0600

    add employee service updated

--- frontend/src/services/employee.service.js
+++ frontend/src/services/employee.service.js
@@ -1,38 +1,38 @@
-// Import from the env 
-const api_url = process.env.VITE_API_URL;
+// Import from the env
+const api_url = import.meta.env.VITE_REACT_APP_API_URL;
 
-// A function to send post request to create a new employee 
+// A function to send post request to create a new employee
 const createEmployee = async (formData, loggedInEmployeeToken) => {
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-access-token': loggedInEmployeeToken
-    },
-    body: JSON.stringify(formData)
-  };
-  console.log(requestOptions);
-  const response = await fetch(`${api_url}/api/employee`, requestOptions);
-  return response;
-}
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"x-access-token": loggedInEmployeeToken,
+		},
+		body: JSON.stringify(formData),
+	};
+	console.log(requestOptions);
+	const response = await fetch(`${api_url}/api/employee`, requestOptions);
+	return response;
+};
 
 // A function to send get request to get all employees
 const getAllEmployees = async (token) => {
-  // console.log(token);
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-access-token': token
-    }
-  };
-  const response = await fetch(`${api_url}/api/employees`, requestOptions);
-  return response;
-}
+	// console.log(token);
+	const requestOptions = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"x-access-token": token,
+		},
+	};
+	const response = await fetch(`${api_url}/api/employees`, requestOptions);
+	return response;
+};
 
-// Export all the functions 
+// Export all the functions
 const employeeService = {
-  createEmployee,
-  getAllEmployees
-}
-export default employeeService; 
+	createEmployee,
+	getAllEmployees,
+};
+export default employeeService;
